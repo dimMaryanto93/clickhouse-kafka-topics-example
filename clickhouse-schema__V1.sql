c165d5c4b4c83fcea4367f2e5ee8d6eafad68d5b
dimasm93 <dimas.maryanto@tabeldata.com>
2023-01-31 12:52:58 +0700

    update docker compose schema version

--- clickhouse-schema__V1.sql
+++ clickhouse-schema__V1.sql
@@ -5,21 +5,20 @@ CREATE TABLE readings
     date        ALIAS toDate(time),
     temperature Decimal(5, 2) Codec (T64, LZ4)
 ) Engine = MergeTree
-      PARTITION BY toYYYYMM(time)
-      ORDER BY (readings_id, time);
+      ORDER BY time;
 
 CREATE TABLE readings_queue
 (
     readings_id Int32,
     time        DateTime,
     temperature Decimal(5, 2)
-)
-    ENGINE = Kafka
-        SETTINGS kafka_broker_list = 'kafka:9092',
-            kafka_topic_list = 'readings',
-            kafka_group_name = 'readings_consumer_group1',
-            kafka_format = 'CSV',
-            kafka_max_block_size = 1048576;
+) ENGINE = Kafka
+      SETTINGS
+          kafka_broker_list = 'kafka:9092',
+          kafka_topic_list = 'readings',
+          kafka_group_name = 'group1',
+          kafka_format = 'CSV',
+          kafka_format = 'JSONAsString';
 
 CREATE MATERIALIZED VIEW readings_queue_mv TO readings AS
 SELECT readings_id, time, temperature
@@ -31,3 +30,11 @@ ATTACH TABLE readings_queue;
 
 select *
 from readings;
+
+TRUNCATE TABLE readings;
+TRUNCATE table readings_queue;
+TRUNCATE table readings_queue_mv;
+
+DROP TABLE readings;
+DROP TABLE readings_queue;
+DROP VIEW readings_queue_mv;
