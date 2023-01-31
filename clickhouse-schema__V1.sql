afec3e014a32f82c5a527e4fa98a88163bb4e8d5
dimasm93 <dimas.maryanto@tabeldata.com>
2023-01-31 16:48:05 +0700

    update configuration

--- clickhouse-schema__V1.sql
+++ clickhouse-schema__V1.sql
@@ -12,27 +12,26 @@ CREATE TABLE readings_queue
     readings_id Int32,
     time        DateTime,
     temperature Decimal(5, 2)
-) ENGINE = Kafka
+) ENGINE = Kafka()
       SETTINGS
           kafka_broker_list = 'kafka:9092',
           kafka_topic_list = 'readings',
-          kafka_group_name = 'group1',
-          kafka_format = 'CSV',
-          kafka_format = 'JSONAsString';
+          kafka_group_name = 'reading_group',
+          kafka_format = 'JSONEachRow',
+          kafka_thread_per_consumer = 0,
+          kafka_num_consumers = 1;
 
 CREATE MATERIALIZED VIEW readings_queue_mv TO readings AS
 SELECT readings_id, time, temperature
 FROM readings_queue;
 
 DETACH TABLE readings_queue;
-
 ATTACH TABLE readings_queue;
 
 select *
 from readings;
 
 TRUNCATE TABLE readings;
-TRUNCATE table readings_queue;
 TRUNCATE table readings_queue_mv;
 
 DROP TABLE readings;
